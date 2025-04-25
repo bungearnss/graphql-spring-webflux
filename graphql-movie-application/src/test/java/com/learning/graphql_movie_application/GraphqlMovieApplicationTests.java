e254c37cf150783356412de236ee857b4306f334
bungearntun <love2554f@hotmail.com>
2025-04-25 11:09:58 +0700

    added : initialize movie application

---
+++ graphql-movie-application/src/test/java/com/learning/graphql_movie_application/GraphqlMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.graphql_movie_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
